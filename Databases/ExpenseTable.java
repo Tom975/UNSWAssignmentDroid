2677a662758f5640e3b716508ef60533b51597f1
Hayden345 <31378460+Hayden345@users.noreply.github.com>
2017-10-24 16:06:58 +1100

    Add files via upload

--- Databases/ExpenseTable.java
+++ Databases/ExpenseTable.java
@@ -11,17 +11,20 @@ public final class ExpenseTable {
 
     public static class ExpenseTableEntry implements BaseColumns {
         public static final String TABLE_NAME = "Expenses";
+        public static final String COLUMN_NAME_LOGBOOK = "Log_Book";
         public static final String COLUMN_NAME_DATE = "Date";
         public static final String COLUMN_NAME_COST = "Cost";
         public static final String COLUMN_NAME_REASON = "Reason";
         public static final String COLUMN_NAME_WORK = "Work";
-        public static final String COLUMN_NAME_RECEIPT = "Receipt Location";
+        public static final String COLUMN_NAME_RECEIPT = "Receipt_Location";
+        public static final String CONSTRAINT_NAME_DRIVER = "fk_driver";
     }
 
     public static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + ExpenseTableEntry.TABLE_NAME +
-            " (" + ExpenseTableEntry._ID + "INTEGER PRIMARY KEY," + ExpenseTableEntry.COLUMN_NAME_DATE + " DATE," +
-            ExpenseTableEntry.COLUMN_NAME_COST + " DECIMAL(10,2)," + ExpenseTableEntry.COLUMN_NAME_REASON + " TEXT," +
-            ExpenseTableEntry.COLUMN_NAME_WORK + " BOOLEAN," + ExpenseTableEntry.COLUMN_NAME_RECEIPT + " TEXT)";
+            " (" + ExpenseTableEntry._ID + "INTEGER PRIMARY KEY," + ExpenseTableEntry.COLUMN_NAME_LOGBOOK + "INTEGER, " + ExpenseTableEntry.COLUMN_NAME_DATE + " DATE," +
+    ExpenseTableEntry.COLUMN_NAME_COST + " DECIMAL(10,2)," + ExpenseTableEntry.COLUMN_NAME_REASON + " TEXT," +
+    ExpenseTableEntry.COLUMN_NAME_WORK + " BOOLEAN," + ExpenseTableEntry.COLUMN_NAME_RECEIPT + " TEXT, CONSTRAINT " + ExpenseTableEntry.CONSTRAINT_NAME_DRIVER + " FOREIGN KEY (" +
+    ExpenseTableEntry.COLUMN_NAME_LOGBOOK + ") REFERENCES " + DriverTable.DriverTableEntry.TABLE_NAME + " (" + DriverTable.DriverTableEntry._ID + "))";
 
     public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + ExpenseTableEntry.TABLE_NAME;
 }
--- Databases/TripTable.java
+++ Databases/TripTable.java
@@ -11,17 +11,19 @@ public class TripTable {
 
     public static class TripTableEntry implements BaseColumns {
         public static final String TABLE_NAME = "Trips";
+        public static final String COLUMN_NAME_LOGBOOK = "Log_Book";
         public static final String COLUMN_NAME_REGISTRATION = "Registration";
         public static final String COLUMN_NAME_MAKE = "Make";
         public static final String COLUMN_NAME_MODEL = "Model";
         public static final String COLUMN_NAME_YEAR = "Year";
         public static final String COLUMN_NAME_ENGINE = "Engine";
+        public static final String CONSTRAINT_NAME_LOGBOOK = "fk_logbook";
     }
 
     public static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + TripTableEntry.TABLE_NAME +
-            " (" + TripTableEntry._ID + "INTEGER PRIMARY KEY," + TripTableEntry.COLUMN_NAME_REGISTRATION + " TEXT," +
-            TripTableEntry.COLUMN_NAME_MAKE + " TEXT," + TripTableEntry.COLUMN_NAME_MODEL + " TEXT," +
-            TripTableEntry.COLUMN_NAME_YEAR + " DATE," + TripTableEntry.COLUMN_NAME_ENGINE + "TEXT)";
+            " (" + TripTableEntry._ID + "INTEGER PRIMARY KEY, " + TripTableEntry.COLUMN_NAME_LOGBOOK + " INTEGER, " + TripTableEntry.COLUMN_NAME_REGISTRATION + " TEXT, " +
+            TripTableEntry.COLUMN_NAME_MAKE + " TEXT, " + TripTableEntry.COLUMN_NAME_MODEL + " TEXT, " + TripTableEntry.COLUMN_NAME_YEAR + " DATE, " + TripTableEntry.COLUMN_NAME_ENGINE + "TEXT, CONSTRAINT " +
+            TripTableEntry.CONSTRAINT_NAME_LOGBOOK + " FOREIGN KEY (" + TripTableEntry.COLUMN_NAME_LOGBOOK + ") REFERENCES " + LogTable.LogTableEntry.TABLE_NAME + " (" + LogTable.LogTableEntry._ID + "))";
 
     public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TripTableEntry.TABLE_NAME;
 }

c72caa844bd1dc660ee80241cb9e6e2cc8c7e46b
Dylan420 <33333051+Dylan420@users.noreply.github.com>
2017-11-04 20:49:30 +1100

    Add files via upload

--- java/com/example/dylan/taxtimewithfriends/manualAddTrip.java
+++ java/com/example/dylan/taxtimewithfriends/manualAddTrip.java
@@ -4,8 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+
+import com.example.dylan.taxtimewithfriends.Objects.Log;
+import com.example.dylan.taxtimewithfriends.Objects.Trip;
 
 public class manualAddTrip extends AppCompatActivity {
+
+    String manWork = "Personal";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +27,27 @@ public class manualAddTrip extends AppCompatActivity {
     
     public void clickFunction(View view){
 
-        EditText manOdoStart = (EditText)findViewById(getResources(R.id.manualOdoStart));
+        EditText manOdoStart = (EditText)findViewById(R.id.manualOdoStart);
 
-        EditText manOdoEnd = (EditText)findViewById(getResources(R.id.manualOdoEnd));
+        EditText manOdoEnd = (EditText)findViewById(R.id.manualOdoEnd);
 
-        EditText manDate = (EditText)findViewById(getResources(R.id.manualDatgetResources(e));
+        EditText manDate = (EditText)findViewById(R.id.manualDate);
 
-        EditText manReason = (EditText)findViewById(getResources(R.id.manualReason));
-        
-        Log.i("Manual Trip added", manOdoStart.getText().toString(), manOdoEnd.getText().toString(), manDate.getText().toString(), manReason.getText().toString());
+        EditText manReason = (EditText)findViewById(R.id.manualReason);
 
+        Switch aSwitch = (Switch)findViewById(R.id.manualSwitch);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                    manWork = "Personal";
+                else manWork = "Business";
+            }
+        });
+
+
+        Trip trip = new Trip(manOdoStart, manOdoEnd, manDate, manDate, manReason, manWork, manLog);
+        db.execSQL(Trip.toSQLInsert(trip));
     }
-
 }

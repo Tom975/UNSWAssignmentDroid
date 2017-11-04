6e88ed75ee6e60b910f147306343904bb54bdb1e
Hayden345 <31378460+Hayden345@users.noreply.github.com>
2017-11-04 18:16:11 +1100

    Update manualAddTrip.java

--- java/com/example/dylan/taxtimewithfriends/manualAddTrip.java
+++ java/com/example/dylan/taxtimewithfriends/manualAddTrip.java
@@ -16,7 +16,19 @@ public class manualAddTrip extends AppCompatActivity {
     public void mainMenuActivity (View v){
         startActivity(new Intent(manualAddTrip.this, MainActivity.class));
     }
+    
+    public void clickFunction(View view){
 
+        EditText manOdoStart = (EditText)findViewById(getResources(R.id.manualOdoStart));
 
+        EditText manOdoEnd = (EditText)findViewById(getResources(R.id.manualOdoEnd));
+
+        EditText manDate = (EditText)findViewById(getResources(R.id.manualDatgetResources(e));
+
+        EditText manReason = (EditText)findViewById(getResources(R.id.manualReason));
+        
+        Log.i("Manual Trip added", manOdoStart.getText().toString(), manOdoEnd.getText().toString(), manDate.getText().toString(), manReason.getText().toString());
+
+    }
 
 }
